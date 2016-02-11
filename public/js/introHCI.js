e4b72da9126f387e012528cea3fdcce83dbe2b39
Jesse Qin <itzjesse@gmail.com>
2016-02-11 01:04:27 -0800

    Asdf

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -65,7 +65,7 @@
         }, 'jsonp');
         $.get('/palette', function (res) {
             console.log(res.colors);
-            var colors = res[0].colors.hex;
+            var colors = res.colors.hex;
             $('body').css('background-color', colors[0]);
             $('.thumbnail').css('background-color', colors[1]);
             $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
