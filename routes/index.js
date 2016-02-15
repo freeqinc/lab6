ef5cab92a244df4b65619d2474d57b5ae6bcfa9a
Jesse Qin <itzjesse@gmail.com>
2016-02-15 15:59:34 -0800

    Asgewa

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,54 @@
  */
 
 exports.view = function(req, res) {
+    if (req.query.test === "yes") {
+        res.render('index', {
+            'projects': [{
+                'name': 'Waiting in Line',
+                'image': 'lorempixel.people.1.jpeg',
+                'id': 'project1'
+            }, {
+                'name': 'Needfinding',
+                'image': 'lorempixel.city.1.jpeg',
+                'id': 'project2'
+            }, {
+                'name': 'Prototyping',
+                'image': 'lorempixel.technics.1.jpeg',
+                'id': 'project3'
+            }, {
+                'name': 'Heuristic Evaluation',
+                'image': 'lorempixel.abstract.1.jpeg',
+                'id': 'project4'
+            }, {
+                'name': 'Skeleton and a Plan',
+                'image': 'lorempixel.abstract.8.jpeg',
+                'id': 'project5'
+            }, {
+                'name': 'Meat on the Bones',
+                'image': 'lorempixel.people.2.jpeg',
+                'id': 'project6'
+            }, {
+                'name': 'Ready for Testing',
+                'image': 'lorempixel.technics.2.jpeg',
+                'id': 'project7'
+            }, {
+                'name': 'User Test Results and Online Test Proposal',
+                'image': 'lorempixel.city.2.jpeg',
+                'id': 'project8'
+            }],
+            'poopers': [{
+                'poop': 'yes'
+            }, {
+                'poop': 'no'
+            }],
+            "helpers": {
+                'foo': function () { console.log("DIFFERENT"); }
+            }
+        });
+        res.redirect('/');
+        return;
+    }
+
     res.render('index', {
         'projects': [{
             'name': 'Waiting in Line',
@@ -36,6 +84,14 @@ exports.view = function(req, res) {
             'name': 'User Test Results and Online Test Proposal',
             'image': 'lorempixel.city.2.jpeg',
             'id': 'project8'
-        }]
+        }],
+        'poopers': [{
+            'poop': 'yes'
+        }, {
+            'poop': 'no'
+        }],
+        "helpers": {
+            'foo': function () { console.log(this.projects[0].name); }
+        }
     });
 };
